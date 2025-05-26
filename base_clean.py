a7ae1769909cff083bb222ff37009a6751e11e45
V0latix <romaind.67@outlook.fr>
2025-05-26 23:17:51 +0200

    stat de population

---
+++ base_clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Charger le CSV
+df = pd.read_csv("effectif_age_sexe.csv", sep=";")
+
+# Supprimer les colonnes vides (colonnes où toutes les valeurs sont nulles)
+df = df.dropna(axis=1, how='all')
+
+# Sauvegarder dans un nouveau fichier propre
+df.to_csv("effectif_age_sexe_2025.csv", sep=";", index=False)
