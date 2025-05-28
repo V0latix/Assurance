d7df93a6c390f0c8758de27fb4f7bf74b1ac2dee
V0latix <romaind.67@outlook.fr>
2025-05-28 09:11:22 +0200

    génération age x sex

--- base_clean.py
+++ base_clean.py
@@ -6,5 +6,7 @@ df = pd.read_csv("effectif_age_sexe.csv", sep=";")
 # Supprimer les colonnes vides (colonnes où toutes les valeurs sont nulles)
 df = df.dropna(axis=1, how='all')
 
+df = df[:-1]
+
 # Sauvegarder dans un nouveau fichier propre
 df.to_csv("effectif_age_sexe_2025.csv", sep=";", index=False)
